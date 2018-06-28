73bd922f391c54f45e34182fb5c3d92e69434f26
rajankumar <rajankumartpss@gmail.com>
2018-06-28 14:18:03 +0530

    first commit

---
+++ map.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="/google-map-php/css/style.css">
+  </head>
+  <body>
+    <div id="map"></div>
+    <script src="/google-map-php/js/googlemap.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA6UH9w9O9ZtSyEGH86UPm76MQf_YDNAWA&callback=initMap"
+        async defer></script>
+  </body>
+</html>
